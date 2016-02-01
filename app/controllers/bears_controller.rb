6d4f9869598d22c0c11c6acefa63ca11bd3a1cfe
Julia Herron <julia.herron@gmail.com>
2016-02-01 08:31:55 -0800

    now we have bears and links

--- app/controllers/bears_controller.rb
+++ app/controllers/bears_controller.rb
@@ -17,10 +17,10 @@ class BearsController < ApplicationController
   def create
     @bear = Bear.new(whitelisted_bear_params)
     if @bear.save
-      flash[:success] = "Bear #{@bear.name} successfully created"
+      flash[:success] = "Bear '#{@bear.name}' successfully created"
       redirect_to @bear
     else
-      flash.now[:error] = "Bear #{@bear.name} was not created"
+      flash.now[:error] = "Bear '#{@bear.name}' was not created"
       render :new
     end
   end
@@ -33,10 +33,10 @@ class BearsController < ApplicationController
   def update
     @bear = Bear.find(params[:id])
     if @bear.update(whitelisted_bear_params)
-      flash[:success] = "Bear #{@bear.name} successfully updated"
+      flash[:success] = "Bear '#{@bear.name}' successfully updated"
       redirect_to @bear
     else
-      flash[:error] = "Bear #{@bear.name} was not updated"
+      flash[:error] = "Bear '#{@bear.name}' was not updated"
       render :edit
     end
   end
@@ -46,10 +46,10 @@ class BearsController < ApplicationController
   def destroy
     @bear = Bear.find(params[:id])
     if @bear.destroy
-      flash[:success] = "Bear #{@bear.name} deleted"
+      flash[:success] = "Bear '#{@bear.name}' deleted"
       redirect_to bears_path
     else
-      flash.now[:error] = "Bear #{@bear.name} was not deleted."
+      flash.now[:error] = "Bear '#{@bear.name}' was not deleted."
       render :show
     end
 
